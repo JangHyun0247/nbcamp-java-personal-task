7492b9f9788120da0432376e7f8df25468f6b401
Robbie <robbie331@>
2024-04-23 17:59:06 +0900

    Revert "Level 1 -1"
    This reverts commit 168c21eaa73181f0c16c4644a21e0ca5fe332d65.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,4 @@
 package calculator;
-
 import java.util.Scanner;
 
 public class App {
@@ -8,24 +7,11 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        //음수를 입력하면 양수로 입력하라는 창이 나오고 다시 입력하라는 창
-        int num1;
-        do {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            num1 = sc.nextInt();
-            if (num1<0){
-                System.out.print("양의 정수를 입력해주세요!  ");
-            }
-        } while (num1 < 0);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
 
-        int num2;
-        do {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = sc.nextInt();
-            if (num2<0){
-                System.out.print("양의 정수를 입력해주세요!  ");
-            }
-        } while (num2 < 0);
 
     }
 }
